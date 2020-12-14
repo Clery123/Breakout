1358a6785e5b67561f3962cb1300285264510571
any name <andrejko.zidek@gmail.com>
2020-12-14 11:32:59 +0100

    add

--- app/src/main/java/com/example/breakout/MainActivity.java
+++ app/src/main/java/com/example/breakout/MainActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Random;
 import java.util.Timer;
@@ -16,7 +19,9 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     private Button buttonPlay;
     private Button buttonLevels;
+    private EditText editText;
     private Button buttonScore;
+    public static String playerName;
     public static SharedPreferences pref;
 
     private Game game;
@@ -25,12 +30,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         pref  = getApplicationContext().getSharedPreferences("MyPref", 0);
         setContentView(R.layout.menu);
+        editText = (EditText)findViewById(R.id.pname);
+        editText.setText(playerName);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                playerName = editText.getText().toString();
+                System.out.println(playerName);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         buttonPlay = findViewById(R.id.play);
         buttonPlay.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                System.out.println("kek");
                 Intent intent = new Intent(getApplicationContext(),Game.class);
                 startActivity(intent);
                 finish();
@@ -41,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                System.out.println("kekovička");
                 Intent intent = new Intent(getApplicationContext(),Levels.class);
                 startActivity(intent);
                 finish();
@@ -59,69 +82,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /*public void Colision(){
 
-        if(breakout.ball_PosY<breakout.height*(float)0.45){
-            for(int i = 0;i<breakout.posLen;i++){
-                if((breakout.ball_PosX >= breakout.pos[i][0] && breakout.ball_PosX <= breakout.pos[i][0]+220 && breakout.ball_PosY>=breakout.pos[i][1]+30 && breakout.ball_PosY<=breakout.pos[i][1]+65 &&breakout.gravity <0)){
-                    if(breakout.bounce <0 && breakout.ball_PosX <= breakout.pos[i][0]+220 && breakout.ball_PosX >= breakout.pos[i][0]+190){
-                        breakout.bounce = -breakout.bounce;
-                        System.out.println("zboku");
-                        float x = breakout.pos[i][0];
-                        float y = breakout.pos[i][1];
-                        breakout.pos[i][0] = breakout.pos[breakout.posLen - 1][0];
-                        breakout.pos[breakout.posLen - 1][0] = x;
-
-                        breakout.pos[i][1] = breakout.pos[breakout.posLen - 1][1];
-                        breakout.pos[breakout.posLen - 1][1] = y;
-                        breakout.posLen -= 1;
-                        break;
-                    }
-                    else {
-                        System.out.println("cheche");
-                        System.out.println(breakout.ball_PosX);
-                        breakout.gravity = -breakout.gravity;
-                        float x = breakout.pos[i][0];
-                        float y = breakout.pos[i][1];
-                        breakout.pos[i][0] = breakout.pos[breakout.posLen - 1][0];
-                        breakout.pos[breakout.posLen - 1][0] = x;
-
-                        breakout.pos[i][1] = breakout.pos[breakout.posLen - 1][1];
-                        breakout.pos[breakout.posLen - 1][1] = y;
-                        breakout.posLen -= 1;
-                        break;
-                    }
-                }
-                else if(breakout.ball_PosX >= breakout.pos[i][0]-30 && breakout.ball_PosX <= breakout.pos[i][0]+220 && breakout.ball_PosY>=breakout.pos[i][1]-45 && breakout.ball_PosY<=breakout.pos[i][1]+25 &&breakout.gravity >0){
-                    if(breakout.bounce >0 && breakout.ball_PosX >= breakout.pos[i][0]-35 && breakout.ball_PosX <= breakout.pos[i][0]){
-                        breakout.bounce = -breakout.bounce;
-                        System.out.println("MLASDM");
-                        float x = breakout.pos[i][0];
-                        float y = breakout.pos[i][1];
-                        breakout.pos[i][0] = breakout.pos[breakout.posLen - 1][0];
-                        breakout.pos[breakout.posLen - 1][0] = x;
-
-                        breakout.pos[i][1] = breakout.pos[breakout.posLen - 1][1];
-                        breakout.pos[breakout.posLen - 1][1] = y;
-                        breakout.posLen -= 1;
-                        break;
-                    }
-                    else {
-                        System.out.println("cheche2");
-                        System.out.println(breakout.bounce);
-                        breakout.gravity = -breakout.gravity;
-                        float x = breakout.pos[i][0];
-                        float y = breakout.pos[i][1];
-                        breakout.pos[i][0] = breakout.pos[breakout.posLen - 1][0];
-                        breakout.pos[breakout.posLen - 1][0] = x;
-
-                        breakout.pos[i][1] = breakout.pos[breakout.posLen - 1][1];
-                        breakout.pos[breakout.posLen - 1][1] = y;
-                        breakout.posLen -= 1;
-                        break;
-                    }
-            }
-            }
-        }
-    }*/
 }
--- app/src/main/java/com/example/breakout/Score.java
+++ app/src/main/java/com/example/breakout/Score.java
@@ -14,13 +14,16 @@ public class Score extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.score);
         textView = (EditText) findViewById(R.id.score);
         textView.setActivated(false);
+        textView.setEnabled(false);
+        textView.setClickable(false);
         for(int i =0;i<index;i++) {
             int a = MainActivity.pref.getInt(String.valueOf(i), MODE_APPEND);
-            textView.append("Score: "+Integer.toString(a)+"\n");
+            textView.append(MainActivity.playerName+": "+a+"\n");
         }
     }
     private EditText textView;
